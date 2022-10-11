f63396f57cfad380d956b143625ea55b26026bf6
Samir Ahmad <ahmadsamir832104@gmail.com>
2022-10-11 09:43:31 +0530

    Initial commit with Min max in array solved

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Data Structures and Algorithms
+This repository will contain the DSA question solved in python
+
+1. Arrays
+2. Strings
+3. Matrices
+4. Searching and Sorting
+5. Backtracking
+6. Linked List
+7. Stack & Queues
+8. Greedy Algorithm
+9. Binary Trees
+10. Binary Search Trees
+11. Heaps & Hashing
+12. Graphs
+13. Dynamic Programming
+14. Bit Manipulation
+15. Segment Trees
---
+++ arrays/minimum-and-maximum/find_min_max.py
@@ -0,0 +1,21 @@
+import json
+
+def find_min_max(arr):
+    if len(arr) == 0:
+        return None, None
+    min_val = arr[0]
+    max_val = arr[0]
+    for value in arr:
+        if value < min_val:
+            min_val = value
+        if value > max_val:
+            max_val = value
+            
+    return min_val, max_val
+
+if __name__ == "__main__":
+    arr = [7, 5, 21, 3, 4, 1, 19]
+    min_value, max_value = find_min_max(arr)
+    print(f"Array items: {arr}")
+    print(f"Minimum: {min_value}")
+    print(f"Maximum: {max_value}")
